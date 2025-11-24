a20b4b80d9085095afe498aa86fda3473310d63c
María Lucía Cisneros <m.l.cisneros.pazos@gmail.com>
2025-11-24 14:20:25 -0500

    Update: CargaPaginada Memoriales

--- src/main/java/org/example/springboot_backend/repository/MemorialRepository.java
+++ src/main/java/org/example/springboot_backend/repository/MemorialRepository.java
@@ -1,14 +1,14 @@
 package org.example.springboot_backend.repository;
-
 import org.example.springboot_backend.entity.Memorial;
 import org.example.springboot_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
-import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface MemorialRepository extends JpaRepository<Memorial, UUID> {
     List<Memorial> findByUser(User user);
@@ -23,5 +23,6 @@ public interface MemorialRepository extends JpaRepository<Memorial, UUID> {
     
     // Find user's personal reflection space (journal)
     Memorial findByUserAndIsJournalTrue(User user);
+    Page<Memorial> findByUserAndIsJournalFalse(User user, Pageable pageable);
 
 }
--- src/main/java/org/example/springboot_backend/service/IMemorialService.java
+++ src/main/java/org/example/springboot_backend/service/IMemorialService.java
@@ -5,12 +5,13 @@ import java.util.List;
 import org.example.springboot_backend.dto.MemorialRequest;
 import org.example.springboot_backend.dto.MemorialResponse;
 import org.example.springboot_backend.entity.User;
+import org.springframework.data.domain.Page;
 import org.springframework.web.multipart.MultipartFile;
 
 
 public interface IMemorialService {
     MemorialResponse createMemorial(MemorialRequest memorial, MultipartFile file, User user);
-    List<MemorialResponse> getMyMemorials(User user);
+    Page<MemorialResponse> getMyMemorials(User user, int page, int size);
     List<MemorialResponse> getCollaborativeMemorials(User user);
     MemorialResponse getMemorialById(String memorialId, User user);
     MemorialResponse updateMemorial(String memorialId, MemorialRequest request, MultipartFile file, User user);
